22c0f346ecc6c150b5b50b2c375163f950fa6255
vanshika vanshika <anandvanshika8@gmail.com>
2023-08-08 23:11:28 -0400

    added banner, rows, data from api

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav">
+      {/* <img src="https://upload.wikimedia.org/wikipedia/commons/0/" alt="Netflix logo" class="nav_logo"> */}
+    </div>
+  );
+}
+
+export default Nav;
